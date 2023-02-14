5a19562b7a7ecb853bd4a0de312f385ceb183124
Rohan Mukherjee <roerohan@gmail.com>
2023-02-14 18:10:39 +0530

    fix: use tsup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import { GithubReporter } from './src';
+// import { GithubReporter } from './src';
+import { GithubReporter } from './dist/index.mjs';
 
 export default defineConfig({
     test: {
