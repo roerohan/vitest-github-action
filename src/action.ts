f650e13cba55c5bfa16e19050d478c27e1700e99
Rohan Mukherjee <roerohan@gmail.com>
2023-02-16 18:59:45 +0530

    fix: log config file

--- src/action.ts
+++ src/action.ts
@@ -1,8 +1,14 @@
 import * as core from '@actions/core';
+import { exec } from 'child_process';
+import { readFile } from 'fs/promises';
+import type { UserConfig } from 'vitest';
 
 async function main(): Promise<void> {
 	const configFile: string = core.getInput('config');
 	console.log(configFile);
+
+	const config: UserConfig = await readFile(configFile);
+	console.log(config);
 }
 
 void main();
