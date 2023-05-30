5357ce19d1b3441fe50900b77d83898b20528236
Rohan Mukherjee <roerohan@gmail.com>
2023-05-30 18:18:57 +0530

    feat: use entire istanbul coverage file

--- src/coverage/GithubIstanbulCoverageProviderModule.ts
+++ src/coverage/GithubIstanbulCoverageProviderModule.ts
@@ -2,10 +2,12 @@ import type {
 	CoverageProviderModule,
 	ReportContext,
 	ResolvedCoverageOptions,
+	AfterSuiteRunMeta,
+	CoverageIstanbulOptions,
+	Vitest,
 } from 'vitest';
 import type {CoverageProvider} from 'vitest';
 
-import {IstanbulCoverageProvider} from '@vitest/coverage-istanbul/dist/provider';
 import {type CoverageMap} from 'istanbul-lib-coverage';
 import libCoverage from 'istanbul-lib-coverage';
 import libSourceMaps from 'istanbul-lib-source-maps';
@@ -13,6 +15,16 @@ import libReport from 'istanbul-lib-report';
 import reports from 'istanbul-reports';
 import GithubIstanbulCoverageReporter from './GithubIstanbulCoverageReporter';
 import GithubSummaryIstanbulCoverageReporter from './GithubSummaryIstanbulCoverageReporter';
+import {existsSync, promises as fs} from 'node:fs';
+import {relative, resolve} from 'pathe';
+import {
+	coverageConfigDefaults,
+	defaultExclude,
+	defaultInclude,
+} from 'vitest/config';
+import {BaseCoverageProvider} from 'vitest/coverage';
+import {type Instrumenter, createInstrumenter} from 'istanbul-lib-instrument';
+import _TestExclude from 'test-exclude';
 import github from '@actions/github';
 
 const githubIstanbulCoverageProviderModule: CoverageProviderModule = {
@@ -22,19 +34,113 @@ const githubIstanbulCoverageProviderModule: CoverageProviderModule = {
 
 	// Implements rest of the CoverageProviderModule ...
 };
-
+export const coverageStorageKey = '__VITEST_COVERAGE__';
 type Options = ResolvedCoverageOptions<'istanbul'>;
 
 export type Octokit = ReturnType<typeof github.getOctokit>;
 export type Github = typeof github;
 const octokit: Octokit = github.getOctokit(process.env?.GITHUB_TOKEN ?? '');
 
-export class GithubIstanbulCoverageProvider extends IstanbulCoverageProvider {
+export class GithubIstanbulCoverageProvider
+	extends BaseCoverageProvider
+	implements CoverageProvider {
 	name = 'github-istanbul';
 
-	declare options: Options;
+	ctx!: Vitest;
+	options!: Options;
+	instrumenter!: Instrumenter;
+	testExclude!: InstanceType<TestExclude>;
 
+	/**
+	 * Coverage objects collected from workers.
+	 * Some istanbul utilizers write these into file system instead of storing in memory.
+	 * If storing in memory causes issues, we can simply write these into fs in `onAfterSuiteRun`
+	 * and read them back when merging coverage objects in `onAfterAllFilesRun`.
+	 */
 	declare coverages: libCoverage.CoverageMap[];
+
+	initialize(ctx: Vitest) {
+		const config: CoverageIstanbulOptions = ctx.config.coverage;
+
+		this.ctx = ctx;
+		this.options = {
+			...coverageConfigDefaults,
+
+			// User's options
+			...config,
+
+			// Resolved fields
+			provider: 'istanbul',
+			reportsDirectory: resolve(
+				ctx.config.root,
+				config.reportsDirectory || coverageConfigDefaults.reportsDirectory,
+			),
+			reporter: this.resolveReporters(
+				config.reporter || coverageConfigDefaults.reporter,
+			),
+		};
+
+		this.instrumenter = createInstrumenter({
+			produceSourceMap: true,
+			autoWrap: false,
+			esModules: true,
+			compact: false,
+			coverageVariable: coverageStorageKey,
+			// @ts-expect-error missing type
+			coverageGlobalScope: 'globalThis',
+			coverageGlobalScopeFunc: false,
+			ignoreClassMethods: this.options.ignoreClassMethods,
+		});
+
+		this.testExclude = new _TestExclude({
+			cwd: ctx.config.root,
+			include:
+				typeof this.options.include === 'undefined'
+					? undefined
+					: [...this.options.include],
+			exclude: [...defaultExclude, ...defaultInclude, ...this.options.exclude],
+			excludeNodeModules: true,
+			extension: this.options.extension,
+		});
+	}
+
+	resolveOptions() {
+		return this.options;
+	}
+
+	onFileTransform(sourceCode: string, id: string, pluginCtx: any) {
+		if (!this.testExclude.shouldInstrument(id)) {
+			return;
+		}
+
+		const sourceMap = pluginCtx.getCombinedSourcemap();
+		sourceMap.sources = sourceMap.sources.map(removeQueryParameters);
+
+		const code = this.instrumenter.instrumentSync(
+			sourceCode,
+			id,
+			sourceMap,
+		);
+		const map = this.instrumenter.lastSourceMap() as any;
+
+		return {code, map};
+	}
+
+	onAfterSuiteRun({coverage}: AfterSuiteRunMeta) {
+		this.coverages.push(coverage);
+	}
+
+	async clean(clean = true) {
+		if (clean && existsSync(this.options.reportsDirectory)) {
+			await fs.rm(this.options.reportsDirectory, {
+				recursive: true,
+				force: true,
+				maxRetries: 10,
+			});
+		}
+
+		this.coverages = [];
+	}
 
 	async reportCoverage({allTestsRun}: ReportContext = {}) {
 		const mergedCoverage: CoverageMap = this.coverages.reduce(
@@ -120,6 +226,110 @@ export class GithubIstanbulCoverageProvider extends IstanbulCoverageProvider {
 			});
 		}
 	}
+
+	checkThresholds(
+		coverageMap: CoverageMap,
+		thresholds: Record<Threshold, number | undefined>,
+	) {
+		// Construct list of coverage summaries where thresholds are compared against
+		const summaries = this.options.perFile
+			? coverageMap.files().map((file: string) => ({
+				file,
+				summary: coverageMap.fileCoverageFor(file).toSummary(),
+			}))
+			: [
+				{
+					file: null,
+					summary: coverageMap.getCoverageSummary(),
+				},
+			];
+
+		// Check thresholds of each summary
+		for (const {summary, file} of summaries) {
+			for (const thresholdKey of [
+				'lines',
+				'functions',
+				'statements',
+				'branches',
+			] as const) {
+				const threshold = thresholds[thresholdKey];
+
+				if (threshold !== undefined) {
+					const coverage = summary.data[thresholdKey].pct;
+
+					if (coverage < threshold) {
+						process.exitCode = 1;
+
+						/*
+						 * Generate error message based on perFile flag:
+						 * - ERROR: Coverage for statements (33.33%) does not meet threshold (85%) for src/math.ts
+						 * - ERROR: Coverage for statements (50%) does not meet global threshold (85%)
+						 */
+						let errorMessage = `ERROR: Coverage for ${thresholdKey} (${coverage}%) does not meet`;
+
+						if (!this.options.perFile) {
+							errorMessage += ' global';
+						}
+
+						errorMessage += ` threshold (${threshold}%)`;
+
+						if (this.options.perFile && file) {
+							errorMessage += ` for ${relative('./', file).replace(
+								/\\/g,
+								'/',
+							)}`;
+						}
+
+						console.error(errorMessage);
+					}
+				}
+			}
+		}
+	}
+
+	async includeUntestedFiles(coverageMap: CoverageMap) {
+		// Load, instrument and collect empty coverages from all files which
+		// are not already in the coverage map
+		const includedFiles = await this.testExclude.glob(this.ctx.config.root);
+		const uncoveredFiles = includedFiles
+			.map(file => resolve(this.ctx.config.root, file))
+			.filter(file => !coverageMap.data[file]);
+
+		const transformResults = await Promise.all(
+			uncoveredFiles.map(async filename => {
+				const transformResult = await this.ctx.vitenode.transformRequest(
+					filename,
+				);
+				return {transformResult, filename};
+			}),
+		);
+
+		for (const {transformResult, filename} of transformResults) {
+			const sourceMap = transformResult?.map;
+
+			if (sourceMap) {
+				this.instrumenter.instrumentSync(
+					transformResult.code,
+					filename,
+					sourceMap,
+				);
+
+				const lastCoverage = this.instrumenter.lastFileCoverage();
+				if (lastCoverage) {
+					coverageMap.addFileCoverage(lastCoverage);
+				}
+			}
+		}
+	}
+}
+
+/**
+ * Remove possible query parameters from filenames
+ * - From `/src/components/Header.component.ts?vue&type=script&src=true&lang.ts`
+ * - To `/src/components/Header.component.ts`
+ */
+function removeQueryParameters(filename: string) {
+	return filename.split('?')[0];
 }
 
 function isEmptyCoverageRange(range: libCoverage.Range) {
