b68766d721bd02603c796c7f5d75e57b0fc15a3f
Rohan Mukherjee <roerohan@gmail.com>
2023-06-01 16:19:07 +0530

    fix: use coverage module from current path

--- src/action.ts
+++ src/action.ts
@@ -1,5 +1,6 @@
 import {getInput} from '@actions/core';
 import {startVitest} from 'vitest/node';
+import {join} from 'path';
 import GithubReporter from './GithubReporter';
 
 async function main(): Promise<void> {
@@ -15,7 +16,7 @@ async function main(): Promise<void> {
 			coverage: {
 				enabled: coverage,
 				provider: 'custom',
-				customProviderModule: 'github-istanbul-coverage-provider',
+				customProviderModule: join(__dirname, 'github-istanbul-coverage-provider'),
 			},
 		},
 	});
